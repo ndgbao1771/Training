cadc30c8eb001473ce87a9f60942f32910279526
BaoDiep <nguyendiepgiabao1771@gmail.com>
2024-01-08 15:51:21 +0700

    commit create repository

---
+++ Training.Data/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Training.Repository.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        T FindById(int Id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
